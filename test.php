e732045c3d1f1f650e97c4ce9bb338ea047c6659
Sergey Telpuk <telpuk.sergey@gmail.com>
2019-05-09 18:44:38 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,27 +1,87 @@
 <?php
-
-/**
- * This file contains an example of coding styles.
- */
-
 declare(strict_types=1);
-
-namespace Vendor\Package;
-
-use Vendor\Package\{ClassA as A, ClassB, ClassC as C};
-use Vendor\Package\SomeNamespace\ClassD as D;
-use Vendor\Package\AnotherNamespace\ClassE as E;
-
-use function Vendor\Package\{functionA, functionB, functionC};
-use function Another\Vendor\functionD;
-
-use const Vendor\Package\{CONSTANT_A, CONSTANT_B, CONSTANT_C};
-use const Another\Vendor\CONSTANT_D;
-
 /**
- * FooBar is an example class.
+ * test.
+ *
+ * @license   MIT
+ * @author    tet
  */
-class FooBar
+namespace Cycle\ORM\Config;
+use Cycle\ORM\Exception\ConfigException;
+use Cycle\ORM\Relation;
+use Cycle\ORM\Select;
+use Spiral\Core\Container\Autowire;
+use Spiral\Core\InjectableConfig;
+final class test extends InjectableConfig
 {
-    // ... additional PHP code ...
+    public const LOADER = 'loader';
+    public const RELATION = 'relation';
+    public const SCHEMA = 'schema';
+    /**
+     * @var array
+     */
+    protected $config = [];
+    /**
+     * @param int|string $type
+     *
+     * @return Autowire
+     */
+    public function getLoader($type): Autowire
+    {
+        if (!isset($this->config[$type][self::LOADER])) {
+            throw new ConfigException("Unable to get relation loader `{$type}`.");
+        }
+        return new Autowire($this->config[$type][self::LOADER]);
+    }
+    /**
+     * @param int|string $type
+     *
+     * @return Autowire
+     */
+    public function getRelation($type): Autowire
+    {
+        if (!isset($this->config[$type][self::RELATION])) {
+            throw new ConfigException("Unable to get relation `{$type}`.");
+        }
+        return new Autowire($this->config[$type][self::RELATION]);
+    }
+    /**
+     * @return RelationConfig
+     */
+    public static function getDefault()
+    {
+        return new static([
+            Relation::HAS_ONE            => [
+                self::LOADER   => Select\Loader\HasOneLoader::class,
+                self::RELATION => Relation\HasOne::class,
+            ],
+            Relation::BELONGS_TO         => [
+                self::LOADER   => Select\Loader\BelongsToLoader::class,
+                self::RELATION => Relation\BelongsTo::class,
+            ],
+            Relation::REFERS_TO          => [
+                self::LOADER   => Select\Loader\BelongsToLoader::class,
+                self::RELATION => Relation\RefersTo::class,
+            ],
+            Relation::HAS_MANY           => [
+                self::LOADER   => Select\Loader\HasManyLoader::class,
+                self::RELATION => Relation\HasMany::class,
+            ],
+            Relation::MANY_TO_MANY       => [
+                self::LOADER   => Select\Loader\ManyToManyLoader::class,
+                self::RELATION => Relation\ManyToMany::class,
+            ],
+            Relation::MORPHED_HAS_ONE    => [
+                self::LOADER   => Select\Loader\Morphed\MorphedHasOneLoader::class,
+                self::RELATION => Relation\Morphed\MorphedHasOne::class,
+            ],
+            Relation::MORPHED_HAS_MANY   => [
+                self::LOADER   => Select\Loader\Morphed\MorphedHasManyLoader::class,
+                self::RELATION => Relation\Morphed\MorphedHasMany::class,
+            ],
+            Relation::BELONGS_TO_MORPHED => [
+                self::RELATION => Relation\Morphed\BelongsToMorphed::class,
+            ],
+        ]);
+    }
 }
