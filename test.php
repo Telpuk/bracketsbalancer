4d6215eaa01b890ce2df4249242a096f4c426140
Sergey Telpuk <telpuk.sergey@gmail.com>
2019-05-09 15:38:33 +0000

    Apply fixes from StyleCI

--- test.php
+++ test.php
@@ -1,28 +1,33 @@
 <?php
 /**
- * test
+ * test.
  *
  * @license   MIT
  * @author    tet
  */
 declare(strict_types=1);
+
 namespace Cycle\ORM\Config;
+
 use Cycle\ORM\Exception\ConfigException;
 use Cycle\ORM\Relation;
 use Cycle\ORM\Select;
 use Spiral\Core\Container\Autowire;
 use Spiral\Core\InjectableConfig;
-final class RelationConfig extends InjectableConfig
+
+final class test extends InjectableConfig
 {
-    public const LOADER   = 'loader';
+    public const LOADER = 'loader';
     public const RELATION = 'relation';
-    public const SCHEMA   = 'schema';
+    public const SCHEMA = 'schema';
     /**
      * @var array
      */
     protected $config = [];
+
     /**
      * @param int|string $type
+     *
      * @return Autowire
      */
     public function getLoader($type): Autowire
@@ -30,10 +35,13 @@ final class RelationConfig extends InjectableConfig
         if (!isset($this->config[$type][self::LOADER])) {
             throw new ConfigException("Unable to get relation loader `{$type}`.");
         }
+
         return new Autowire($this->config[$type][self::LOADER]);
     }
+
     /**
      * @param int|string $type
+     *
      * @return Autowire
      */
     public function getRelation($type): Autowire
@@ -41,8 +49,10 @@ final class RelationConfig extends InjectableConfig
         if (!isset($this->config[$type][self::RELATION])) {
             throw new ConfigException("Unable to get relation `{$type}`.");
         }
+
         return new Autowire($this->config[$type][self::RELATION]);
     }
+
     /**
      * @return RelationConfig
      */
@@ -51,35 +61,35 @@ final class RelationConfig extends InjectableConfig
         return new static([
             Relation::HAS_ONE            => [
                 self::LOADER   => Select\Loader\HasOneLoader::class,
-                self::RELATION => Relation\HasOne::class
+                self::RELATION => Relation\HasOne::class,
             ],
             Relation::BELONGS_TO         => [
                 self::LOADER   => Select\Loader\BelongsToLoader::class,
-                self::RELATION => Relation\BelongsTo::class
+                self::RELATION => Relation\BelongsTo::class,
             ],
             Relation::REFERS_TO          => [
                 self::LOADER   => Select\Loader\BelongsToLoader::class,
-                self::RELATION => Relation\RefersTo::class
+                self::RELATION => Relation\RefersTo::class,
             ],
             Relation::HAS_MANY           => [
                 self::LOADER   => Select\Loader\HasManyLoader::class,
-                self::RELATION => Relation\HasMany::class
+                self::RELATION => Relation\HasMany::class,
             ],
             Relation::MANY_TO_MANY       => [
                 self::LOADER   => Select\Loader\ManyToManyLoader::class,
-                self::RELATION => Relation\ManyToMany::class
+                self::RELATION => Relation\ManyToMany::class,
             ],
             Relation::MORPHED_HAS_ONE    => [
                 self::LOADER   => Select\Loader\Morphed\MorphedHasOneLoader::class,
-                self::RELATION => Relation\Morphed\MorphedHasOne::class
+                self::RELATION => Relation\Morphed\MorphedHasOne::class,
             ],
             Relation::MORPHED_HAS_MANY   => [
                 self::LOADER   => Select\Loader\Morphed\MorphedHasManyLoader::class,
-                self::RELATION => Relation\Morphed\MorphedHasMany::class
+                self::RELATION => Relation\Morphed\MorphedHasMany::class,
             ],
             Relation::BELONGS_TO_MORPHED => [
-                self::RELATION => Relation\Morphed\BelongsToMorphed::class
-            ]
+                self::RELATION => Relation\Morphed\BelongsToMorphed::class,
+            ],
         ]);
     }
 }
