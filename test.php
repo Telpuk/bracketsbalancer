f3510364644f1ba27d6b2bc14f2f8c602583672b
Sergey Telpuk <telpuk.sergey@gmail.com>
2019-05-14 19:55:17 +0300

    Update test.php

--- test.php
+++ test.php
@@ -14,74 +14,21 @@ use Spiral\Core\Container\Autowire;
 use Spiral\Core\InjectableConfig;
 final class test extends InjectableConfig
 {
-    public const LOADER = 'loader';
-    public const RELATION = 'relation';
-    public const SCHEMA = 'schema';
-    /**
-     * @var array
-     */
-    protected $config = [];
-    /**
-     * @param int|string $type
-     *
-     * @return Autowire
-     */
-    public function getLoader($type): Autowire
+ public function create(PropertiesInterface $properties): Response
     {
-        if (!isset($this->config[$type][self::LOADER])) {
-            throw new ConfigException("Unable to get relation loader `{$type}`.");
-        }
-        return new Autowire($this->config[$type][self::LOADER]);
-    }
-    /**
-     * @param int|string $type
-     *
-     * @return Autowire
-     */
-    public function getRelation($type): Autowire
-    {
-        if (!isset($this->config[$type][self::RELATION])) {
-            throw new ConfigException("Unable to get relation `{$type}`.");
-        }
-        return new Autowire($this->config[$type][self::RELATION]);
-    }
-    /**
-     * @return RelationConfig
-     */
-    public static function getDefault()
-    {
-        return new static([
-            Relation::HAS_ONE            => [
-                self::LOADER   => Select\Loader\HasOneLoader::class,
-                self::RELATION => Relation\HasOne::class,
-            ],
-            Relation::BELONGS_TO         => [
-                self::LOADER   => Select\Loader\BelongsToLoader::class,
-                self::RELATION => Relation\BelongsTo::class,
-            ],
-            Relation::REFERS_TO          => [
-                self::LOADER   => Select\Loader\BelongsToLoader::class,
-                self::RELATION => Relation\RefersTo::class,
-            ],
-            Relation::HAS_MANY           => [
-                self::LOADER   => Select\Loader\HasManyLoader::class,
-                self::RELATION => Relation\HasMany::class,
-            ],
-            Relation::MANY_TO_MANY       => [
-                self::LOADER   => Select\Loader\ManyToManyLoader::class,
-                self::RELATION => Relation\ManyToMany::class,
-            ],
-            Relation::MORPHED_HAS_ONE    => [
-                self::LOADER   => Select\Loader\Morphed\MorphedHasOneLoader::class,
-                self::RELATION => Relation\Morphed\MorphedHasOne::class,
-            ],
-            Relation::MORPHED_HAS_MANY   => [
-                self::LOADER   => Select\Loader\Morphed\MorphedHasManyLoader::class,
-                self::RELATION => Relation\Morphed\MorphedHasMany::class,
-            ],
-            Relation::BELONGS_TO_MORPHED => [
-                self::RELATION => Relation\Morphed\BelongsToMorphed::class,
-            ],
-        ]);
+        [
+            'create_api' => $api
+        ] = $this->hubSpotConfig->get('contact');
+
+        $request = new Request(
+            'POST',
+            $this->hubSpotHttpClientService->getUriWithAuth($api),
+            ['Content-Type' => 'application/json'],
+            $this->jsonEncoder->encode(
+                ['properties' => $properties->getProperties()]
+            )
+        );
+
+        return $this->hubSpotHttpClientService->request($request);
     }
 }
